d63350a7b06541773ea0c30783f4aec8544d52a8
JustinForBiac <jdeponthier@biac.lu>
2020-02-17 16:20:54 +0100

    Merge index.ts (store)

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,12 +4,8 @@ import { RootState } from './types';
 import { articles } from './articles/index';
 import { authentificationModule } from './authentification/index';
 import { modeModule } from './modes/index';
-<<<<<<< HEAD
 import {fournisseursModule} from './fournisseurs/index'
 
-=======
-import { UserModule } from './user/index';
->>>>>>> 50ce210d90cd99605d7f5e19891c6e1dc6775e14
 
 Vue.use(Vuex);
 
@@ -23,11 +19,7 @@ const store: StoreOptions<RootState> = {
     articles,
     authentificationModule,
     modeModule,
-<<<<<<< HEAD
     fournisseursModule,
-=======
-    UserModule,
->>>>>>> 50ce210d90cd99605d7f5e19891c6e1dc6775e14
   },
 };
 
